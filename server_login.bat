05e1df1a84fa17c02fc7e19b790900a80d22403c
Kenichi Ishii <kenih4@gmail.com>
2025-04-11 15:37:34 +0900

    Init

---
+++ server_login.bat
@@ -0,0 +1,15 @@
+net use * /delete
+net use
+net use \\saclaopr18.spring8.or.jp\common /user:sesopr ses@sacla5712
+net use \\saclaopr18.spring8.or.jp\common /user:SPRING8\xfelopr xfel5712
+
+
+
+
+rem	ログノートサーバー
+rem	http://saclaopr19.spring8.or.jp/~lognote/calendar/gantt-group-tasks-together.html
+rem	net use \\saclaoprfs01.spring8.or.jp /user:SPRING8\xfelopr xfel5712
+net use \\saclaoprfs01.spring8.or.jp /user:xfelopr xfel5712
+
+
+pause
