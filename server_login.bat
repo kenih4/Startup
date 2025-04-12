26d36d2e4e0ed627f8c51e3977215878c0dccc0c
Kenichi Ishii <kenih4@gmail.com>
2025-04-12 20:05:11 +0900

    ネット接続を確認するように変更

--- server_login.bat
+++ server_login.bat
@@ -1,18 +1,29 @@
-net use * /delete
-net use
-net use \\saclaopr18.spring8.or.jp\common /user:sesopr ses@sacla5712
-net use \\saclaopr18.spring8.or.jp\common /user:SPRING8\xfelopr xfel5712
 
 
 
+curl -s --head http://www.google.com >nul
+if %errorlevel%==0 (
+    echo Success
 
-rem	LOG-Note & Calendar Server
-rem	http://saclaopr19.spring8.or.jp/~lognote/calendar/gantt-group-tasks-together.html
-rem	net use \\saclaoprfs01.spring8.or.jp /user:SPRING8\xfelopr xfel5712
-net use \\saclaoprfs01.spring8.or.jp /user:xfelopr xfel5712
+    net use * /delete
+
+    net use \\saclaopr18.spring8.or.jp\common /user:sesopr ses@sacla5712
+    net use \\saclaopr18.spring8.or.jp\common /user:SPRING8\xfelopr xfel5712
+
+    rem	LOG-Note & Calendar Server
+    rem	http://saclaopr19.spring8.or.jp/~lognote/calendar/gantt-group-tasks-together.html
+    rem	net use \\saclaoprfs01.spring8.or.jp /user:SPRING8\xfelopr xfel5712
+    net use \\saclaoprfs01.spring8.or.jp /user:xfelopr xfel5712
+
+    rem SMBv1を一時的に有効にしないといけない
+    net use \\sesaccfs2.spring8.or.jp\operation /user:linac linac
+
+    net use
+
+) else (
+    echo Fail
+)
 
 
-rem pingは通るが、、、、　　　ping sesaccfs2.spring8.or.jp    SMBv1を一時的に有効にしないといけない
-net use \\sesaccfs2.spring8.or.jp\operation /user:linac linac
 
 pause
